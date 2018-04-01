a4a3d4aea50b7a1f6d2842cf86665647f3c31d43
ZhaoZhiYing <31733268+ZhaoZhiYing@users.noreply.github.com>
2018-04-01 17:57:53 +0800

    first commit

---
+++ av.js
@@ -0,0 +1,9 @@
+//初始化代码
+var APP_ID = '0mKg6fXQaQ7kihFoUFm5i76F-gzGzoHsz';
+var APP_KEY = 'iKWt6xdjft5RTNRQyd0jPw8T';
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY,
+    // 启用美国节点
+    region: 'us'
+});
